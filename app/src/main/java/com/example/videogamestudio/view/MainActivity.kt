d9c0d082497c9e85d869e793c64fa335baf3b5f2
Josa <al375530@uji.es>
2021-05-28 01:12:04 +0200

    Token fail.

--- app/src/main/java/com/example/videogamestudio/view/MainActivity.kt
+++ app/src/main/java/com/example/videogamestudio/view/MainActivity.kt
@@ -18,11 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var buscar:Button
 
-    var token:String=""
+    var token=""
 
-
-
-
+    val queue = Volley.newRequestQueue(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,26 +29,14 @@ class MainActivity : AppCompatActivity() {
         buscar=findViewById(R.id.buscar)
 
         // Obtenemos el token
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://id.twitch.tv/oauth2/token"+
-                "?client_id=ct0gepnwg8kekcdqu41c3z5nj0agrh"+
-                "&client_secret=0kmngavgupurbrvc1nydekhug7jnok"+
-                "&grant_type=client_credentials"
+        getToken({ token ->
+            if (token!=null){
+                this.token = token
 
-
-
-        // Request a string response from the provided URL.
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.POST, url, null,
-            Response.Listener<JSONObject>
-            { response ->
-                // Display the first 500 characters of the response string.
-                token=Response.Listener<String>
-            },
-            { error -> buscar.text = "That didn't work!" })
-
-        // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+            } else{
+                Log.d("token", "No token")
+            }
+        }, { error -> Log.d("token","No token")})
 
         buscar.setOnClickListener {
             // Instantiate the RequestQueue.
@@ -76,14 +62,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun getCountries(listener: Response.Listener<List<Country>>, errorListener: Response.ErrorListener){
-        val url="$BASE_URL/$COUNTRIES"
-        Log.d("COVIDStats", "El URL es $url")
+    fun getToken(listener: Response.Listener<String>, errorListener: Response.ErrorListener) {
+        val url = "https://id.twitch.tv/oauth2/token"+
+                "?client_id=ct0gepnwg8kekcdqu41c3z5nj0agrh"+
+                "&client_secret=0kmngavgupurbrvc1nydekhug7jnok"+
+                "&grant_type=client_credentials"
         val jsonObjectRequest=JsonObjectRequest(Request.Method.GET, url, null,
-            { response -> processCountries(response, listener)},
-            { error -> errorListener.onErrorResponse(error) })
+            { response -> var token = ""
+                val tokenArray = response.getJSONArray("access_token")
+                token = tokenArray[0] as String
+                Log.d("token", "Token: ${token}")
+            },
+            { error -> buscar.text = "That didn't work!" })
         queue.add(jsonObjectRequest)
     }
-
-
 }
