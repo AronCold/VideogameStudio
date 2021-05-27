f4201d42cc72260ee6e75d731772a0b858f33c9e
AronCold <al286322@uji.es>
2021-05-27 04:16:59 +0200

    identificacion para api

--- app/src/main/java/com/example/videogamestudio/network/Network.kt
+++ app/src/main/java/com/example/videogamestudio/network/Network.kt
@@ -1,13 +1,14 @@
 package com.example.videogamestudio.network
 
 import android.content.Context
+import com.android.volley.toolbox.Volley
 
 
-private const val BASE_URL= "https://api.igdb.com/v4/games/"
-private const val ID="id"
+//private const val BASE_URL= "https://api.igdb.com/v4/games/"
+//private const val ID="id"
 
 class Network private constructor(context: Context){
     companion object: SingletonHolder<Network, Context>(::Network)
 
-    val queue = Volley.newRequestQueue(context)
+    //val queue = Volley.newRequestQueue(context)
 }
--- app/src/main/java/com/example/videogamestudio/view/MainActivity.kt
+++ app/src/main/java/com/example/videogamestudio/view/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.example.videogamestudio.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.videogamestudio.R
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var buscar:Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buscar=findViewById(R.id.buscar)
+
+        buscar.setOnClickListener {
+            // Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://id.twitch.tv/oauth2/token"+
+                    "?client_id=ct0gepnwg8kekcdqu41c3z5nj0agrh"+
+                    "&client_secret=0kmngavgupurbrvc1nydekhug7jnok"+
+                    "&grant_type=client_credentials"
+
+            // Request a string response from the provided URL.
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.POST, url, null,
+                Response.Listener<JSONObject>
+                { response ->
+                    // Display the first 500 characters of the response string.
+                    buscar.text = "Response is: Todo ha ido bien"
+                },
+                { error -> buscar.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(jsonObjectRequest)
+        }
+
+
+
     }
 }
